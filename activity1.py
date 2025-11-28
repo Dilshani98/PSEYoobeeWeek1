021e061daabd1b1e5273664282534f0ac9a4d1d1
dilshani98 <dilshanisubodha@gmail.com>
2025-11-28 20:26:31 +1300

    activity1 completed

--- activity1.py
+++ activity1.py
@@ -1,15 +1,13 @@
+#Activity 1: Multi Type Profile
 
-#define the variables with values
+#define the variables with values of different data types
 
-Name = "John Doe" #String Value
-Age = 28 #Int Value
-Skills = ["Python", "SQL", "Power BI"] #set
-Education = ("BSc Computer Science", 2020) 
-ContactDetails = {"email": "Dilshani Senanayake", "phone": "0203445656563"} #dict
-Certifications = [
-    ("Azure", "AWS", "Azure"), #this shouldn't be set
-    {"Azure", "AWS"}
-]
+Name = "John Doe" #String
+Age = 28 #Integer
+Skills = ["Python", "SQL", "Power BI"] #list
+Education = ("BSc Computer Science", 2020) #tuple
+ContactDetails = {"email": "Dilshani Senanayake", "phone": "0203445656563"} #dictionary
+Certifications = {"Azure", "AWS", "Azure"} #set ,duplicates will be removed
 
 Data = [Name, Age, Skills, Education, ContactDetails, Certifications]
 
